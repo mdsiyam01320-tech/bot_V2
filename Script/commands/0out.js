0b61172ab2b75441eedfd9c6d0a49ca7b793758c
mdsiyam01320-tech <mdsiyam01320@gmail.com>
2026-07-01 01:06:34 +0600

    Update 0out.js

--- Script/commands/0out.js
+++ Script/commands/0out.js
@@ -2,14 +2,26 @@ module.exports.config = {
     name: "out",
     version: "1.0.0",
     hasPermssion: 2,
-    credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
-    description: "",
+    credits: "𝆠፝𝐒𝐈𝐘𝐀𝐌-𝐇𝐀𝐒𝐀𝐍",
+    description: "Bot leaves the group with custom message",
     commandCategory: "Admin",
     usages: "out [id]",
     cooldowns: 10,
 };
 
 module.exports.run = async function({ api, event, args }) {
-        if (!args[0]) return api.removeUserFromGroup(api.getCurrentUserID(), event.threadID);
-        if (!isNaN(args[0])) return api.removeUserFromGroup(api.getCurrentUserID(), args.join(" "));
+    const msg = `───────────────\n» 🤲 আসসালামু আলাইকুম\n» আমি 𝆠፝𝗡𝗜𝗝𝗛𝗨𝗠-𝗖𝗛𝗔𝗧-𝗕𝗢𝗧\n» 💌 আমাকে ব্যবহার করার জন্য ধন্যবাদ 😘\n» ⚠️ আলবিদা সবাই! আমি এখন গ্রুপ থেকে বের হচ্ছি...😞\n───────────────\n» 👤 𝆠፝𝐒𝐈𝐘𝐀𝐌-𝐇𝐀𝐒𝐀𝐍`;
+
+    if (!args[0]) {
+        return api.sendMessage(msg, event.threadID, () => {
+            api.removeUserFromGroup(api.getCurrentUserID(), event.threadID);
+        });
+    }
+    
+    if (!isNaN(args[0])) {
+        const targetThreadID = args.join(" ");
+        return api.sendMessage(msg, targetThreadID, () => {
+            api.removeUserFromGroup(api.getCurrentUserID(), targetThreadID);
+        });
+    }
 }
