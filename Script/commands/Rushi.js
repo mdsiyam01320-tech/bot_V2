68f9e3b305cb71d1bff16df8f16391cb858f02ce
mdsiyam01320-tech <mdsiyam01320@gmail.com>
2026-07-01 02:06:24 +0600

    Update Rushi.js

--- Script/commands/Rushi.js
+++ Script/commands/Rushi.js
@@ -1,30 +1,50 @@
+const axios = require('axios');
+const fs = require("fs-extra");
+
 module.exports.config = {
  name: "rushia",
- version: "1.0.0",
+ version: "1.0.1",
  hasPermssion: 0,
- credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
- description: "Random Rushia",
+ credits: "𝆠፝𝐒𝐈𝐘𝐀𝐌-𝐇𝐀𝐒𝐀𝐍",
+ description: "Random Rushia images",
  commandCategory: "random-img",
- usages: "rushia",
- cooldowns: 3
+ usages: "ব্যবহারের নিয়ম: rushia",
+ cooldowns: 3,
+ dependencies: {
+    "axios": "",
+    "fs-extra": ""
+ }
 };
 
 module.exports.run = async ({ api, event }) => {
- const axios = require('axios');
- const request = require('request');
- const fs = require("fs");
- axios.get('https://saikiapi-v3-production.up.railway.app/holo/rushia').then(res => {
- let ext = res.data.url.substring(res.data.url.lastIndexOf(".") + 1);
- let callback = function () {
-     api.sendMessage({
-      attachment: fs.createReadStream(__dirname + `/cache/rushia.${ext}`)
-     }, event.threadID, () => fs.unlinkSync(__dirname + `/cache/rushia.${ext}`), event.messageID);
-   api.setMessageReaction("✅", event.messageID, (err) => {}, true);
-    };
-    request(res.data.url).pipe(fs.createWriteStream(__dirname + `/cache/rushia.${ext}`)).on("close", callback);
-   })
-      .catch(err => {
-                     api.sendMessage("there's something problem while generating photo, please try again!", event.threadID, event.messageID);
-    api.setMessageReaction("☹️", event.messageID, (err) => {}, true);
-                  })    
+ const { threadID, messageID } = event;
+ 
+ try {
+
+  const res = await axios.get('https://saikiapi-v3-production.up.railway.app/holo/rushia');
+  const imageUrl = res.data.url;
+  const ext = imageUrl.substring(imageUrl.lastIndexOf(".") + 1);
+  const pathImg = __dirname + `/cache/rushia.${ext}`;
+  
+  
+  const imageBuffer = (await axios.get(imageUrl, { responseType: 'arraybuffer' })).data;
+  fs.writeFileSync(pathImg, Buffer.from(imageBuffer, 'utf-8'));
+  
+  
+  return api.sendMessage({
+   attachment: fs.createReadStream(pathImg)
+  }, threadID, () => {
+   if (fs.existsSync(pathImg)) fs.unlinkSync(pathImg);
+   api.setMessageReaction("✅", messageID, (err) => {}, true);
+  }, messageID);
+
+ } catch (err) {
+ 
+  api.setMessageReaction("☹️", messageID, (err) => {}, true);
+  return api.sendMessage(
+`───────────────
+» ❌ 𝗧𝗵𝗲𝗿𝗲'𝘀 𝗮 𝗽𝗿𝗼𝗯𝗹𝗲𝗺 𝘄𝗵𝗶𝗹𝗲 𝗴𝗲𝗻𝗲𝗿𝗮𝘁𝗶𝗻𝗴 𝘁𝗵𝗲 𝗽𝗵𝗼𝘁𝗼, 𝗽𝗹𝗲𝗮𝘀𝗲 𝘁𝗿𝘆 𝗮𝗴𝗮𝗶𝗻!
+───────────────
+» 👤 𝆠፝𝐒𝐈𝐘𝐀𝐌-𝐇𝐀𝐒𝐀𝐍`, threadID, messageID);
+ }
 }
